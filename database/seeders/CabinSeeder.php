4445a9999a950b14c9d1c195b2546f4a577aed22
LAPTOP-1HEQTJJ1\xdcam <camilo.linaresm@autonoma.edu.co>
2024-10-28 19:31:04 -0500

    no message

--- database/seeders/CabinSeeder.php
+++ database/seeders/CabinSeeder.php
@@ -13,19 +13,6 @@ class CabinSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    // {
-    //     DB::table('cabins')->insert([
-    //         'name' => 'Cabin1',
-    //         'cabinLevel_id' => 33,
-    //         'capacity' => 4,
-    //     ]);
-
-    //     DB::table('cabins')->insert([
-    //         'name' => 'Cabin2',
-    //         'cabinLevel_id' => 34,
-    //         'capacity' => 6,
-    //     ]);
-    // }
     {
         DB::table('cabins')->insert([
             'name' => 'Cabin10',
@@ -38,19 +25,18 @@ class CabinSeeder extends Seeder
             'cabinLevel_id' => 2,
             'capacity' => 6,
         ]);
-    }
-    DB::table('cabins')->insert([
-        'name' => 'Cabin3',
-        'cabinLevel_id' => 35,
-        'capacity' => 8,
-    ]);
+        DB::table('cabins')->insert([
+            'name' => 'Cabin3',
+            'cabinLevel_id' => 35,
+            'capacity' => 8,
+        ]);
 
-    DB::table('cabins')->insert([
-        'name' => 'Cabin4',
-        'cabinLevel_id' => 36,
+        DB::table('cabins')->insert([
+            'name' => 'Cabin4',
+            'cabinLevel_id' => 36,
             'capacity' => 10,
         ]);
 
-    }   
+    }
 }
        
